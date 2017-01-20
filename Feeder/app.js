54f3e3d8f1dca948281a8616b9646ec3faecf1c0
raja <rpulyala@deslist.onmicrosoft.com>
2017-01-20 00:41:05 -0500

    intial express js structure push

--- Feeder/app.js
+++ Feeder/app.js
@@ -1,61 +1,47 @@
-﻿var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+var path1 = require('path');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
 });
 
 
-module.exports = app;
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+
+router.get("/about", function (req, res) {
+    res.sendFile(path + "about.html");
+});
+
+router.get("/contact", function (req, res) {
+    res.sendFile(path + "contact.html");
+});
+
+
+app.use("/", router);
+
+// first parameter is the mount point, second is the location in the file system
+
+
+//app.use("*", function (req, res) {
+//    res.sendFile(path + "404.html");
+//});
+
+
+
+app.use('/', express.static(path1.join(__dirname + '/public')));
+
+
+
+//router.get('/static', function (req, res) {
+//      res.sendFile(path.join(__dirname, 'public', 'SP.js'));
+//  });
+
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
+});
